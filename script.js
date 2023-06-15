2d8428f3e1059f30028e413e187c1c2e66458cc2
V <natandegrandi@gmail.com>
2023-06-15 15:12:10 -0300

    Complete js training and exercises

--- script.js
+++ script.js
@@ -4,7 +4,47 @@ console.log(message);
 let name, admin;
 name = "john";
 admin = name;
-alert(admin);
+//alert(admin); //Alert stops all code under it's line from being read before you click "OK" -- That's so cool :0
 
 let planetEarth;
 let currentUserName;
+
+const myInt = 5;
+const myFloat = 6.667;
+const myFixedFloat = myFloat.toFixed(7);
+
+console.log(myInt);
+//alert (typeof myInt);
+
+console.log(myFloat);
+//alert (typeof myFloat)
+
+console.log(myFixedFloat);
+
+let myNumber = "74";
+myNumber = Number(myNumber) + 3;
+console.log(myNumber);
+
+//------
+let x = 60 % 3;
+console.log(x);
+
+//------
+let numIncrement = 4;
+numIncrement++;
+console.log(numIncrement);
+
+//------ Exercise 1
+console.log(23 + 97);
+
+//------ Exercise 2
+console.log(23 + 97 + 30 + 29 +69 +420);
+
+//------ Exercise 3
+console.log((4 + 6 + 9) / 77);
+
+//------ Exercise 4
+let a = 10;
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
